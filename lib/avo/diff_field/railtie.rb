94355699d88c41dac75c49aa6204b8e4f66ab35d
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-30 12:33:36 +0300

    refactor: plugin rework

--- lib/avo/diff_field/railtie.rb
+++ lib/avo/diff_field/railtie.rb
@@ -2,7 +2,9 @@ module Avo
   module DiffField
     class Railtie < Rails::Railtie
       initializer "avo-diff_field.init" do
-        Avo.plugin_manager.register Avo::DiffField::Plugin
+        ActiveSupport.on_load(:avo_boot) do
+          Avo.plugin_manager.register_field :diff, Avo::DiffField::Fields::DiffField
+        end
       end
     end
   end
