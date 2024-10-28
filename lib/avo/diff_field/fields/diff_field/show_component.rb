8049d49baac117a73aaf2074ca5c717089c070e7
Paul Bob <paul.ionut.bob@gmail.com>
2024-10-28 17:09:10 +0200

    extend compatibility for paper trail

--- lib/avo/diff_field/fields/diff_field/show_component.rb
+++ lib/avo/diff_field/fields/diff_field/show_component.rb
@@ -4,17 +4,23 @@ class Avo::DiffField::Fields::DiffField::ShowComponent < Avo::Fields::ShowCompon
   def initialize **args
     super(**args)
     @diffy_value = @field.value&.map do |k, v|
-      case @field.record.try(:action) || @field.record.try(:event)
-      when 'update'
+      if @field.record.class == "PaperTrail::Version".safe_constantize
         left_value = v[0]
         right_value = v[1]
-      when 'destroy'
-        left_value = v
-        right_value = ''
-      when 'create'
-        left_value = ''
-        right_value = v
+      elsif @field.record.class == "Audited::Audit".safe_constantize
+        case @field.record.action
+        when 'update'
+          left_value = v[0]
+          right_value = v[1]
+        when 'destroy'
+          left_value = v
+          right_value = ''
+        when 'create'
+          left_value = ''
+          right_value = v
+        end
       end
+
       [k, Diffy::Diff.new(left_value.to_s, right_value.to_s, format: :html, include_plus_and_minus_in_html: true)]
     end
   end
