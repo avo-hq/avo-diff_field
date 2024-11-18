fc01ca2f1757cf3788bb8a8dddeffa452a65b711
Paul Bob <paul.ionut.bob@gmail.com>
2024-11-18 12:09:45 +0200

    reduce compatibility only for paper_trail (temporary)

--- lib/avo/diff_field/fields/diff_field/show_component.rb
+++ lib/avo/diff_field/fields/diff_field/show_component.rb
@@ -4,22 +4,21 @@ class Avo::DiffField::Fields::DiffField::ShowComponent < Avo::Fields::ShowCompon
   def initialize **args
     super(**args)
     @diffy_value = @field.value&.map do |k, v|
-      if @field.record.class == "PaperTrail::Version".safe_constantize
-        left_value = v[0]
-        right_value = v[1]
-      elsif @field.record.class == "Audited::Audit".safe_constantize
-        case @field.record.action
-        when 'update'
-          left_value = v[0]
-          right_value = v[1]
-        when 'destroy'
-          left_value = v
-          right_value = ''
-        when 'create'
-          left_value = ''
-          right_value = v
-        end
-      end
+      left_value = v[0]
+      right_value = v[1]
+      # Logic for audited:
+      #   case action
+      #   when 'update'
+      #     left_value = v[0]
+      #     right_value = v[1]
+      #   when 'destroy'
+      #     left_value = v
+      #     right_value = ''
+      #   when 'create'
+      #     left_value = ''
+      #     right_value = v
+      #   end
+      # end
 
       [k, Diffy::Diff.new(left_value.to_s, right_value.to_s, format: :html, include_plus_and_minus_in_html: true)]
     end
